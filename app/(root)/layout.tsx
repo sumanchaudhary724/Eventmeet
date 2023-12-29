0cefcb377ea76fcea37f45ffb5c9b6a5e5f17c8e
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-12-29 18:33:46 +1100

    Navbar bar done including mobile navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,22 @@
-import Footer from "@/components/shared/Footer";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../globals.css";
 import Header from "@/components/shared/Header";
+import Footer from "@/components/shared/Footer";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Platform to manage Event",
+  icons: {
+    icon: "/assets/images/logo.svg",
+  },
+};
 export default function RootLayout({
   children,
 }: {
