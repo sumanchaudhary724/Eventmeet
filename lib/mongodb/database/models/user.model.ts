cc75afeb1f920210e8c2f53b91e907592500c089
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-12-30 13:03:55 +1100

    updated user.Action

--- lib/mongodb/database/models/user.model.ts
+++ lib/mongodb/database/models/user.model.ts
@@ -1,14 +1,13 @@
 import { Schema, model, models } from "mongoose";
 
-const UserSchema = new Schema({
+const userSchema = new Schema({
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
+  userName: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   photo: { type: String, required: true },
 });
 
-const User = models.User || model("User", UserSchema);
-
+const User = models.User || model("User", userSchema); //get model from mongoose or create a new one if does not exist
 export default User;
